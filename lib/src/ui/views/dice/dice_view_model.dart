379d987457bb2947ab4551f29c6c321c013fc6b5
Simon Attila Weis <simonattilaweis@gmail.com>
2020-09-30 13:12:39 +0200

    Add hive dependencies

--- lib/src/ui/views/dice/dice_view_model.dart
+++ lib/src/ui/views/dice/dice_view_model.dart
@@ -1,14 +1,16 @@
 import 'package:persistent_dicer/src/ui/global/custom_base_view_model.dart';
+import 'package:hive/hive.dart';
 
 class DiceViewModel extends CustomBaseViewModel {
 // jiffy
   List textFragments = [];
 
   void rollDice(String text) {
+    
+    // var box = await Hive.openBox('testBox');
+
     // testerArray = text.split('w');
     // print();
-
-    
 
     textFragments.add({
       'text': text,
