0004e3945251f750342fa435f045c4ab300637ad
Simon Weis <simonattilaweis@tutanota.com>
2020-10-06 21:44:38 +0200

    Roll the dice

--- lib/src/ui/views/dice/dice_view_model.dart
+++ lib/src/ui/views/dice/dice_view_model.dart
@@ -1,23 +1,42 @@
 import 'package:persistent_dicer/src/ui/global/custom_base_view_model.dart';
-import 'package:hive/hive.dart';
+import 'dart:math';
 
 class DiceViewModel extends CustomBaseViewModel {
-// jiffy
   List textFragments = [];
 
   void rollDice(String text) {
-    
-    // var box = await Hive.openBox('testBox');
+    if (!isValid(text)) {
+      printToDiceLog("Ungültiger Wurf");
+    } else {
+      var diceFragments = text.split('w');
+      diceFragments.removeWhere((value) => value == '');
 
-    // testerArray = text.split('w');
-    // print();
-
-    textFragments.add({
-      'text': text,
-    });
-
-    print(textFragments);
-
+      if (diceFragments.length == 1) {
+        getDiceResult(1, int.parse(diceFragments[0]));
+      } else if (diceFragments.length == 2) {
+        getDiceResult(int.parse(diceFragments[0]), int.parse(diceFragments[1]));
+      }
+    }
     notifyListeners();
+  }
+
+  void getDiceResult(int factor, int diceSides) {
+    final _random = new Random();
+    var rolled = 1 + _random.nextInt(diceSides - 1);
+    printToDiceLog(': ' + rolled.toString());
+  }
+
+  bool isValid(String text) {
+    final validCharsAndNumbers = RegExp(r"(\d+)?w(\d+)([\+\-]\d+)?");
+    if (validCharsAndNumbers.hasMatch(text)) {
+      return true;
+    }
+    return false;
+  }
+
+  void printToDiceLog(String textToAdd) {
+    textFragments.add({
+      'text': textToAdd,
+    });
   }
 }
